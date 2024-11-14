70c04d98cd0b9bf552d36518c293ff3e553ae9d3
Arttu Aalto <t1aaar01@students.oamk.fi>
2024-11-14 19:22:23 +0200

    fixes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TextInput, View, Pressable, Modal, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import PieChart from 'react-native-pie-chart';
-import { BarChart } from 'react-native-gifted-charts';
 import { FIREBASE_AUTH, FIRESTORE_DB } from '../database/databaseConfig'; 
 import { getDoc, doc } from 'firebase/firestore'; 
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { onAuthStateChanged } from 'firebase/auth';
 
 const HomeScreen = () => {
-  const [calories, setCalories] = useState('');
-  const [username, setUsername] = useState('User'); 
+  const [calories, setCalories] = useState(0);
+  const [username, setUsername] = useState(''); 
   const [loading, setLoading] = useState(true); 
 
   const widthAndHeight = 200;
@@ -16,37 +16,46 @@ const HomeScreen = () => {
   const sliceColor = ['#fbd203', '#ffb300', '#ff9100', '#ff6c00', '#ff3c00'];
 
   useEffect(() => {
-    const fetchUsername = async () => {
-      const user = FIREBASE_AUTH.currentUser; 
-      if (user) {
-        try {
-          const userRef = doc(FIRESTORE_DB, 'users', user.uid);  
-          const userSnap = await getDoc(userRef);
-          if (userSnap.exists()) {
-            const userData = userSnap.data();
-            setUsername(userData.username); 
-            await AsyncStorage.setItem('userName', userData.username); 
-          } else {
-            setUsername('User'); 
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, async (user) => {  
+      console.log("User state changed:", user);
+
+      const fetchUsername = async () => {
+        if (user) {
+          try {
+            const userRef = doc(FIRESTORE_DB, 'users', user.uid);
+            const userSnap = await getDoc(userRef);
+
+            if (userSnap.exists()) {
+              const userData = userSnap.data();
+              setUsername(userData.username);
+              await AsyncStorage.setItem('userName', userData.username);
+              console.log('Username fetched and stored:', userData.username);
+            } else {
+              setUsername('User');
+              console.log('User document not found, setting default username');
+            }
+          } catch (error) {
+            console.error('Error fetching user data:', error);
+            setUsername('User');
+          } finally {
+            setLoading(false);
           }
-        } catch (error) {
-          console.error("Error getting user data: ", error);
-          setUsername('User');
-        } finally {
+        } else {
+          const storedUsername = await AsyncStorage.getItem('userName');
+          if (storedUsername) {
+            setUsername(storedUsername);
+            console.log('Username fetched from AsyncStorage:', storedUsername);
+          } else {
+            setUsername('User');
+            console.log('No stored username found, setting default username');
+          }
           setLoading(false);
         }
-      } else {
-        const storedUsername = await AsyncStorage.getItem('userName');
-        if (storedUsername) {
-          setUsername(storedUsername);
-        }
-        setLoading(false);
-      }
-    };
-
-    fetchUsername();
-  }, []); 
-
+      };
+      fetchUsername();
+    });
+    return () => unsubscribe();
+  }, []);
   if (loading) {
     return (
       <View style={styles.container}>
@@ -101,24 +110,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#000',
     top: '40%',
-  },
-  meals: {
-    flexDirection: 'row',
-    marginTop: 20,
-    padding: 10,
-    justifyContent:'space-between',
-  },
-  waterButton: {
-    backgroundColor: 'grey',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row', 
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
   },
 });
 

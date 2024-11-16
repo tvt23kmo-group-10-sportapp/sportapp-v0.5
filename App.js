612d8a7b2e7d263febdf5d0b86fbff340f46ae30
Ville-A <ville.m.ahola@gmail.com>
2024-11-16 21:38:52 +0200

    fixed app

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
 import MainNavigator from './MainNavigator';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
